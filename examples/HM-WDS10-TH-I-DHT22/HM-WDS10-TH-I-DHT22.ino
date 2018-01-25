2b478bbc9ccc7c6c1bd4688e6151189e62883cb2
jp112sdl <jp112sdl@icloud.com>
2018-01-25 22:33:27 +0100

    added own expamples

---
+++ examples/HM-WDS10-TH-I-DHT22/HM-WDS10-TH-I-DHT22.ino
@@ -0,0 +1,155 @@
+//- -----------------------------------------------------------------------------------------------------------------------
+// AskSin++
+// 2016-10-31 papa Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+//- -----------------------------------------------------------------------------------------------------------------------
+
+// define this to read the device id, serial and device type from bootloader section
+// #define USE_OTA_BOOTLOADER
+
+#define EI_NOTEXTERNAL
+#include <EnableInterrupt.h>
+#include <AskSinPP.h>
+#include <LowPower.h>
+#include <DHT.h>
+
+#include <MultiChannelDevice.h>
+
+// we use a Pro Mini
+// Arduino pin for the LED
+// D5 == PIN 5 on Pro Mini
+#define LED_PIN 5
+// D4 == PIN 4 on Pro Mini
+#define DHT22_PIN 4
+// Arduino pin for the config button
+// B0 == PIN 8 on Pro Mini
+#define CONFIG_BUTTON_PIN 8
+
+// number of available peers per channel
+#define PEERS_PER_CHANNEL 6
+
+//seconds between sending messages
+#define MSG_INTERVAL 400
+
+// all library classes are placed in the namespace 'as'
+using namespace as;
+
+DHT dht(DHT22_PIN, DHT22);
+
+// define all device properties
+const struct DeviceInfo PROGMEM devinfo = {
+  {0x34, 0x56, 0x78},     // Device ID
+  "JPTH10I001",           // Device Serial
+  {0x00, 0x3f},           // Device Model
+  0x10,                   // Firmware Version
+  as::DeviceType::THSensor, // Device Type
+  {0x01, 0x00}            // Info Bytes
+};
+
+/**
+   Configure the used hardware
+*/
+typedef AvrSPI<10, 11, 12, 13> SPIType;
+typedef Radio<SPIType, 2> RadioType;
+typedef StatusLed<5> LedType;
+typedef AskSin<LedType, BatterySensor, RadioType> BaseHal;
+class Hal : public BaseHal {
+  public:
+    void init (const HMID& id) {
+      BaseHal::init(id);
+      // measure battery every 1h
+      battery.init(seconds2ticks(60UL * 60), sysclock);
+      battery.low(22);
+      battery.critical(19);
+    }
+
+    bool runready () {
+      return sysclock.runready() || BaseHal::runready();
+    }
+} hal;
+
+class WeatherEventMsg : public Message {
+  public:
+    void init(uint8_t msgcnt, int16_t temp, uint8_t humidity, bool batlow) {
+      uint8_t t1 = (temp >> 8) & 0x7f;
+      uint8_t t2 = temp & 0xff;
+      if ( batlow == true ) {
+        t1 |= 0x80; // set bat low bit
+      }
+      Message::init(0xc, msgcnt, 0x70, BIDI, t1, t2);
+      pload[0] = humidity;
+    }
+};
+
+class WeatherChannel : public Channel<Hal, List1, EmptyList, List4, PEERS_PER_CHANNEL, List0>, public Alarm {
+
+    WeatherEventMsg msg;
+    int16_t         temp;
+    uint8_t         humidity;
+    uint16_t        millis;
+
+  public:
+    WeatherChannel () : Channel(), Alarm(5), temp(0), humidity(0), millis(0) {}
+    virtual ~WeatherChannel () {}
+
+    // here we do the measurement
+    void measure () {
+      DPRINT("Measure...\n");
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
+      while (isnan(t) || isnan(h)) {
+        h = dht.readHumidity();
+        t = dht.readTemperature();
+      }
+      humidity = h;
+      temp = t * 10.0;
+      DPRINT("T/H = " + String(temp) + "/" + String(humidity) + "\n");
+    }
+
+    virtual void trigger (__attribute__ ((unused)) AlarmClock& clock) {
+      uint8_t msgcnt = device().nextcount();
+      // reactivate for next measure
+      tick = delay();
+      clock.add(*this);
+      measure();
+
+      msg.init(msgcnt, temp, humidity, device().battery().low());
+      device().sendPeerEvent(msg, *this);
+    }
+
+    uint32_t delay () {
+      return seconds2ticks(MSG_INTERVAL);
+    }
+
+    void setup(Device<Hal, List0>* dev, uint8_t number, uint16_t addr) {
+      Channel::setup(dev, number, addr);
+      sysclock.add(*this);
+    }
+
+    uint8_t status () const {
+      return 0;
+    }
+
+    uint8_t flags () const {
+      return 0;
+    }
+};
+
+typedef MultiChannelDevice<Hal, WeatherChannel, 1> WeatherType;
+WeatherType sdev(devinfo, 0x20);
+
+ConfigButton<WeatherType> cfgBtn(sdev);
+
+void setup () {
+  DINIT(57600, ASKSIN_PLUS_PLUS_IDENTIFIER);
+  sdev.init(hal);
+  buttonISR(cfgBtn, CONFIG_BUTTON_PIN);
+  sdev.initDone();
+}
+
+void loop() {
+  bool worked = hal.runready();
+  bool poll = sdev.pollRadio();
+  if ( worked == false && poll == false ) {
+    hal.activity.savePower<Sleep<>>(hal);
+  }
+}
