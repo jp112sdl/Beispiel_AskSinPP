b28bb3ccd9b3953c5ee9ef46085aab57c6369848
jp112sdl <jp112sdl@icloud.com>
2018-02-06 19:19:01 +0100

    added example

---
+++ examples/HM-LC-Sw2-FM/HM-LC-Sw2-FM.ino
@@ -0,0 +1,119 @@
+//- -----------------------------------------------------------------------------------------------------------------------
+// AskSin++
+// 2016-10-31 papa Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+//- -----------------------------------------------------------------------------------------------------------------------
+
+// define this to read the device id, serial and device type from bootloader section
+// #define USE_OTA_BOOTLOADER
+
+#define CFG_LOWACTIVE_BYTE 0x00
+#define CFG_LOWACTIVE_ON   0x01
+#define CFG_LOWACTIVE_OFF  0x00
+
+#define DEVICE_CONFIG CFG_LOWACTIVE_OFF
+
+#define EI_NOTEXTERNAL
+#include <EnableInterrupt.h>
+#include <AskSinPP.h>
+#include <LowPower.h>
+
+#include <Switch.h>
+
+// we use a Pro Mini
+// Arduino pin for the LED
+// D4 == PIN 4 on Pro Mini
+#define LED_PIN 4
+// Arduino pin for the config button
+// B0 == PIN 8 on Pro Mini
+#define CONFIG_BUTTON_PIN 8
+
+#define RELAY1_PIN 17
+#define RELAY2_PIN 16
+
+#define BUTTON1_PIN 6
+#define BUTTON2_PIN 3
+
+// number of available peers per channel
+#define PEERS_PER_CHANNEL 8
+
+// all library classes are placed in the namespace 'as'
+using namespace as;
+
+// define all device properties
+const struct DeviceInfo PROGMEM devinfo = {
+  {0x12, 0x34, 0x56},     // Device ID
+  "JPLCSw2001",           // Device Serial
+  {0x00, 0x09},           // Device Model
+  0x24,                   // Firmware Version
+  as::DeviceType::Switch, // Device Type
+  {0x01, 0x00}            // Info Bytes
+};
+
+/**
+   Configure the used hardware
+*/
+typedef AvrSPI<10, 11, 12, 13> RadioSPI;
+typedef AskSin<StatusLed<4>, NoBattery, Radio<RadioSPI, 2> > Hal;
+
+// setup the device with channel type and number of channels
+typedef MultiChannelDevice<Hal, SwitchChannel<Hal, PEERS_PER_CHANNEL, List0>, 4> SwitchType;
+
+Hal hal;
+SwitchType sdev(devinfo, 0x20);
+ConfigButton<SwitchType> cfgBtn(sdev);
+InternalButton<SwitchType> btn1(sdev, 1);
+InternalButton<SwitchType> btn2(sdev, 2);
+
+// if A0 and A1 connected
+// we use LOW for ON and HIGH for OFF
+bool checkLowActive () {
+  pinMode(14, OUTPUT); // A0
+  pinMode(15, INPUT_PULLUP); // A1
+  digitalWrite(15, HIGH);
+  digitalWrite(14, LOW);
+  bool result = digitalRead(15) == LOW;
+  digitalWrite(14, HIGH);
+  return result;
+}
+
+void initPeerings (bool first) {
+  // create internal peerings - CCU2 needs this
+  if ( first == true ) {
+    HMID devid;
+    sdev.getDeviceID(devid);
+    for ( uint8_t i = 1; i <= sdev.channels(); ++i ) {
+      Peer ipeer(devid, i);
+      sdev.channel(i).peer(ipeer);
+    }
+  }
+}
+
+void initModelType () {
+  uint8_t model[2];
+  sdev.getDeviceModel(model);
+  sdev.channels(2);
+}
+
+void setup () {
+  DINIT(57600, ASKSIN_PLUS_PLUS_IDENTIFIER);
+  bool first = sdev.init(hal);
+  bool low = false;
+  sdev.channel(1).init(RELAY1_PIN, low);
+  sdev.channel(2).init(RELAY2_PIN, low);
+
+  buttonISR(cfgBtn, CONFIG_BUTTON_PIN);
+  buttonISR(btn1, BUTTON1_PIN);
+  buttonISR(btn2, BUTTON2_PIN);
+
+  initModelType();
+  initPeerings(first);
+  sdev.initDone();
+}
+
+void loop() {
+  bool worked = hal.runready();
+  bool poll = sdev.pollRadio();
+  if ( worked == false && poll == false ) {
+    hal.activity.savePower<Idle<> >(hal);
+  }
+}
