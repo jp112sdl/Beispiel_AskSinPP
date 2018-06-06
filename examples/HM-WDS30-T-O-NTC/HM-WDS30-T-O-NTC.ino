43fbd133667f710fbd58e1dab95d28eb3c682c75
Alexander Reinert <alex@areinert.de>
2018-06-06 23:32:10 +0200

    Added HM-WDS30-T-O (with NTC sensor)

---
+++ examples/HM-WDS30-T-O-NTC/HM-WDS30-T-O-NTC.ino
@@ -0,0 +1,150 @@
+//- -----------------------------------------------------------------------------------------------------------------------
+// AskSin++
+// 2016-10-31 papa Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+//- -----------------------------------------------------------------------------------------------------------------------
+
+// define this to read the device id, serial and device type from bootloader section
+// #define USE_OTA_BOOTLOADER
+
+#define EI_NOTEXTERNAL
+#include <EnableInterrupt.h>
+#include <AskSinPP.h>
+#include <LowPower.h>
+
+#include <Register.h>
+#include <MultiChannelDevice.h>
+#include <sensors/Ntc.h>
+
+// Arduino Pro mini 8 Mhz
+// Arduino pin for the config button
+#define CONFIG_BUTTON_PIN 8
+
+// number of available peers per channel
+#define PEERS_PER_CHANNEL 6
+
+// send all xx seconds
+#define MSG_INTERVAL 180
+
+// restince both of ntc and known resistor
+#define NTC_T0_RESISTENCE 10000
+// b of ntc (see datasheet)
+#define NTC_B 3435
+
+// pin to measure ntc
+#define NTC_SENSE_PIN 14
+// pin to power ntc (or 0 if connected to vcc)
+#define NTC_ACTIVATOR_PIN 6
+
+// all library classes are placed in the namespace 'as'
+using namespace as;
+
+// define all device properties
+const struct DeviceInfo PROGMEM devinfo = {
+  {0x00, 0x3e, 0x01},          // Device ID
+  "ARTO000001",                // Device Serial
+  {0x00, 0x3e},                // Device Model
+  0x10,                        // Firmware Version
+  as::DeviceType::THSensor,    // Device Type
+  {0x01, 0x01}                 // Info Bytes
+};
+
+/**
+   Configure the used hardware
+*/
+typedef AvrSPI<10, 11, 12, 13> SPIType;
+typedef Radio<SPIType, 2> RadioType;
+typedef StatusLed<4> LedType;
+typedef AskSin<LedType, BatterySensor, RadioType> BaseHal;
+class Hal : public BaseHal {
+  public:
+    void init (const HMID& id) {
+      BaseHal::init(id);
+
+      battery.init(seconds2ticks(60UL * 60), sysclock); //battery measure once an hour
+      battery.low(22);
+      battery.critical(18);
+    }
+
+    bool runready () {
+      return sysclock.runready() || BaseHal::runready();
+    }
+} hal;
+
+class WeatherEventMsg : public Message {
+  public:
+    void init(uint8_t msgcnt, int temp, bool batlow) {
+      uint8_t t1 = (temp >> 8) & 0x7f;
+      uint8_t t2 = temp & 0xff;
+      if ( batlow == true ) {
+        t1 |= 0x80; // set bat low bit
+      }
+      Message::init(0xb, msgcnt, 0x70, (msgcnt % 20 == 1) ? BIDI : BCAST, t1, t2);
+    }
+};
+
+class WeatherChannel : public Channel<Hal, List1, EmptyList, List4, PEERS_PER_CHANNEL, List0>, public Alarm {
+
+    WeatherEventMsg msg;
+
+    Ntc<NTC_SENSE_PIN,NTC_T0_RESISTENCE,NTC_B,NTC_ACTIVATOR_PIN> ntc;
+
+  public:
+    WeatherChannel () : Channel(), Alarm(5) {}
+    virtual ~WeatherChannel () {}
+
+
+    // here we do the measurement
+    void measure () {
+      DPRINT("Measure...\n");
+      ntc.measure();
+      DPRINT("T = ");DDECLN(ntc.temperature());
+    }
+
+    virtual void trigger (__attribute__ ((unused)) AlarmClock& clock) {
+      uint8_t msgcnt = device().nextcount();
+      // reactivate for next measure
+      tick = delay();
+      clock.add(*this);
+      measure();
+
+      msg.init(msgcnt, ntc.temperature(), device().battery().low());
+      device().sendPeerEvent(msg, *this);
+    }
+
+    uint32_t delay () {
+      return seconds2ticks(MSG_INTERVAL);
+    }
+    void setup(Device<Hal, List0>* dev, uint8_t number, uint16_t addr) {
+      Channel::setup(dev, number, addr);
+      ntc.init();
+      sysclock.add(*this);
+    }
+
+    uint8_t status () const {
+      return 0;
+    }
+
+    uint8_t flags () const {
+      return 0;
+    }
+};
+
+typedef MultiChannelDevice<Hal, WeatherChannel, 1> WeatherType;
+WeatherType sdev(devinfo, 0x20);
+ConfigButton<WeatherType> cfgBtn(sdev);
+
+void setup () {
+  DINIT(57600, ASKSIN_PLUS_PLUS_IDENTIFIER);
+  sdev.init(hal);
+  buttonISR(cfgBtn, CONFIG_BUTTON_PIN);
+  sdev.initDone();
+}
+
+void loop() {
+  bool worked = hal.runready();
+  bool poll = sdev.pollRadio();
+  if ( worked == false && poll == false ) {
+    hal.activity.savePower<Sleep<>>(hal);
+  }
+}
+
