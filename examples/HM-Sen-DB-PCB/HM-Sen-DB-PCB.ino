ea8c8badca93b94d0bf36f3a9c5a5df51046ad18
jp112sdl <jp112sdl@icloud.com>
2019-03-01 17:44:50 +0100

    refactored code to send only "short press" and implemented List0 options

--- examples/HM-Sen-DB-PCB/HM-Sen-DB-PCB.ino
+++ examples/HM-Sen-DB-PCB/HM-Sen-DB-PCB.ino
@@ -1,6 +1,7 @@
 //- -----------------------------------------------------------------------------------------------------------------------
 // AskSin++
 // 2017-07-26 papa Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+// 2019-03-01 jp112sdl Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
 //- -----------------------------------------------------------------------------------------------------------------------
 
 // define this to read the device id, serial and device type from bootloader section
@@ -14,16 +15,11 @@
 #include <MultiChannelDevice.h>
 #include <Remote.h>
 
-#define LED_PIN 4
-#define LED_PIN2 5
-
-// Arduino pin for the config button
-// B0 == PIN 8
+#define LED_PIN           4
 #define CONFIG_BUTTON_PIN 8
-// Arduino pins for the buttons
-// A0 == PIN 14
-#define BTN1_PIN 14
-
+#define TRG_PIN           9
+#define SEND_ON_EDGE      FALLING // FALLING -> senden bei Pegelwechsel von HIGH auf LOW
+// RISING  -> senden bei Pegelwechsel von LOW auf HIGH
 
 // number of available peers per channel
 #define PEERS_PER_CHANNEL 10
@@ -33,61 +29,92 @@ using namespace as;
 
 // define all device properties
 const struct DeviceInfo PROGMEM devinfo = {
-    {0x00,0xdc,0x01},       // Device ID
-    "HMJPDB0001",           // Device Serial
-    {0x00,0xDC},            // Device Model
-    0x16,                   // Firmware Version
-    as::DeviceType::Remote, // Device Type
-    {0x01,0x00}             // Info Bytes
+  {0x00, 0xdc, 0x01},     // Device ID
+  "HMJPDB0001",           // Device Serial
+  {0x00, 0xDC},           // Device Model
+  0x16,                   // Firmware Version
+  as::DeviceType::Remote, // Device Type
+  {0x01, 0x00}            // Info Bytes
 };
 
 /**
- * Configure the used hardware
- */
-typedef AvrSPI<10,11,12,13> SPIType;
-typedef Radio<SPIType,2> RadioType;
-typedef DualStatusLed<LED_PIN2, LED_PIN> LedType;
-typedef AskSin<LedType,BatterySensor,RadioType> HalType;
+   Configure the used hardware
+*/
+typedef AvrSPI<10, 11, 12, 13> SPIType;
+typedef Radio<SPIType, 2> RadioType;
+typedef StatusLed<LED_PIN> LedType;
+typedef AskSin<LedType, BatterySensor, RadioType> HalType;
 class Hal : public HalType {
-  // extra clock to count button press events
-  AlarmClock btncounter;
-public:
-  void init (const HMID& id) {
-    HalType::init(id);
-    // get new battery value after 50 key press
-    battery.init(50,btncounter);
-    battery.low(22);
-    battery.critical(19);
-  }
+    // extra clock to count button press events
+    AlarmClock btncounter;
+  public:
+    void init (const HMID& id) {
+      HalType::init(id);
+      // get new battery value after 50 key press
+      battery.init(50, btncounter);
+      battery.low(22);
+      battery.critical(19);
+    }
 
-  void sendPeer () {
-    --btncounter;
-  }
+    void sendPeer () {
+      --btncounter;
+    }
 
-  bool runready () {
-    return HalType::runready() || btncounter.runready();
-  }
+    bool runready () {
+      return HalType::runready() || btncounter.runready();
+    }
 };
 
-typedef RemoteChannel<Hal,PEERS_PER_CHANNEL,List0> ChannelType;
-typedef MultiChannelDevice<Hal,ChannelType,1> RemoteType;
+DEFREGISTER(DBReg0, MASTERID_REGS, DREG_LEDMODE, DREG_TRANSMITTRYMAX, DREG_LOCALRESETDISABLE)
+class DBList0 : public RegList0<DBReg0> {
+  public:
+    DBList0(uint16_t addr) : RegList0<DBReg0>(addr) {}
+    void defaults () {
+      clear();
+      ledMode(1);
+      transmitDevTryMax(6);
+      localResetDisable(false);
+    }
+};
+
+typedef RemoteChannel<Hal, PEERS_PER_CHANNEL, DBList0> ChannelType;
+typedef MultiChannelDevice<Hal, ChannelType, 1, DBList0> RemoteType;
 
 Hal hal;
-RemoteType sdev(devinfo,0x20);
+RemoteType sdev(devinfo, 0x20);
 ConfigButton<RemoteType> cfgBtn(sdev);
 
+volatile bool isr = false;
+static void isr1 () {
+  isr = true;
+}
+
 void setup () {
-  DINIT(57600,ASKSIN_PLUS_PLUS_IDENTIFIER);
+  DINIT(57600, ASKSIN_PLUS_PLUS_IDENTIFIER);
   sdev.init(hal);
-  remoteISR(sdev,1,BTN1_PIN);
-  buttonISR(cfgBtn,CONFIG_BUTTON_PIN);
+
+  pinMode(TRG_PIN, INPUT_PULLUP);
+  if ( digitalPinToInterrupt(TRG_PIN) == NOT_AN_INTERRUPT )
+    enableInterrupt(TRG_PIN, isr1, SEND_ON_EDGE);
+  else
+    attachInterrupt(digitalPinToInterrupt(TRG_PIN), isr1, SEND_ON_EDGE);
+
+  buttonISR(cfgBtn, CONFIG_BUTTON_PIN);
   sdev.initDone();
 }
 
 void loop() {
   bool worked = hal.runready();
   bool poll = sdev.pollRadio();
-  if( worked == false && poll == false ) {
+  if ( worked == false && poll == false ) {
     hal.activity.savePower<Sleep<>>(hal);
   }
+
+  if (isr == true) {
+    isr = false;
+    _delay_ms(50); //DEBOUNCE
+    sdev.channel(1).state(Button::released);
+  }
+
 }
+
