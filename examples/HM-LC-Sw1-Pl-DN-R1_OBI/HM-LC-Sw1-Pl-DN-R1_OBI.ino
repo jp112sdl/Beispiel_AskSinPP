54b466ee1c9e14dc1d24ef5cbee7e390de585546
Jérôme <jp112sdl@icloud.com>
2020-01-24 19:37:07 +0100

    Update HM-LC-Sw1-Pl-DN-R1_OBI.ino

--- examples/HM-LC-Sw1-Pl-DN-R1_OBI/HM-LC-Sw1-Pl-DN-R1_OBI.ino
+++ examples/HM-LC-Sw1-Pl-DN-R1_OBI/HM-LC-Sw1-Pl-DN-R1_OBI.ino
@@ -45,7 +45,7 @@ const struct DeviceInfo PROGMEM devinfo = {
   {0x00, 0xd8, 0x99},     // Device ID
   "JPLCSw1099",           // Device Serial
   {0x00,0xd8},            // Device Model
-  0x25,                   // Firmware Version
+  0x26,                   // Firmware Version
   as::DeviceType::Switch, // Device Type
   {0x01, 0x00}            // Info Bytes
 };
