8edac1d40e70907118e3b231de7ff0db3e063846
jp112sdl <jp112sdl@icloud.com>
2021-01-01 13:07:06 +0100

    update sketch

--- examples/HM-PB-2-FM_RWE_WSC2/HM-PB-2-FM_RWE_WSC2.ino
+++ examples/HM-PB-2-FM_RWE_WSC2/HM-PB-2-FM_RWE_WSC2.ino
@@ -4,54 +4,54 @@
 // 2020-12-26 jp112sdl Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
 //- -----------------------------------------------------------------------------------------------------------------------
 
-// define this to read the device id, serial and device type from bootloader section
-// #define USE_OTA_BOOTLOADER
 //#define USE_AES
 //#define HM_DEF_KEY ...
 //#define HM_DEF_KEY_INDEX 0
 
-//#define NDEBUG
+#define SENSOR_ONLY
+#define SIMPLE_CC1101_INIT
+#define NOCRC
+#define NDEBUG
 
-#define STORAGEDRIVER at24cX<0x50,128,32> 
-#include <Wire.h>
+//external EEPROM, unused in original eQ-3 Firmware 
+//#define STORAGEDRIVER at24cX<0x50,128,32> 
+//#include <Wire.h>
 
 #define EI_NOTEXTERNAL
 #include <EnableInterrupt.h>
 #include <AskSinPP.h>
-#include <LowPower.h>
-
 #include <MultiChannelDevice.h>
 #include <Remote.h>
 
-#define LED          8
-#define CC1101_GDO0  2
-#define CC1101_PWR   5
-#define CONFIG_BTN   0
-#define BTN01_PIN    15
-#define BTN02_PIN    14
-#define ACTIVATE_PIN 9
+#define LED_PIN           8
+#define CC1101_GDO0_PIN   2
+#define CC1101_PWR_PIN    5
+#define CONFIG_BTN        0
+#define BTN01_PIN         15
+#define BTN02_PIN         14
+#define ACTIVATE_PIN      9
 
-#define PEERS_PER_CHANNEL 10
+#define WAKE_TIME_MS      2500   //Stay awake for 2.5 seconds after last action (original eQ-3 -> 10 secs)
+#define MSG_COUNT_BYTE    8     //StorageConfig Byte to save last message counter value
 
-// all library classes are placed in the namespace 'as'
+#define BAT_LOW           22
+#define BAT_CRIT          19
+
+#define PEERS_PER_CHANNEL 16
+
 using namespace as;
-
-// define all device properties
 const struct DeviceInfo PROGMEM devinfo = {
-    {0x00,0xc2,0x01},       // Device ID
+    {0x00,0x36,0x21},       // Device ID
     "JPWSCRWE01",           // Device Serial
-    {0x00,0xc2},            // Device Model
-    0x14,                   // Firmware Version
+    {0x00,0x36},            // Device Model
+    0x10,                   // Firmware Version
     as::DeviceType::Remote, // Device Type
     {0x00,0x00}             // Info Bytes
 };
 
-/**
- * Configure the used hardware
- */
 typedef AvrSPI<10,11,12,13> SPIType;
-typedef Radio<SPIType,CC1101_GDO0> RadioType;
-typedef StatusLed<LED> LedType;
+typedef Radio<SPIType,CC1101_GDO0_PIN> RadioType;
+typedef StatusLed<LED_PIN> LedType;
 typedef AskSin<LedType,IrqInternalBatt,RadioType> Hal;
 
 typedef RemoteChannel<Hal,PEERS_PER_CHANNEL,List0> ChannelType;
@@ -61,32 +61,94 @@ Hal hal;
 RemoteType sdev(devinfo,0x20);
 ConfigButton<RemoteType> cfgBtn(sdev);
 
+template <uint8_t EN_PIN>
+class PowerOffAlarm : public Alarm {
+private:
+  bool    timerActive;
+public:
+  PowerOffAlarm () : Alarm(0), timerActive(false) {}
+  ~PowerOffAlarm () {}
+
+  void init() {
+    pinMode(EN_PIN, OUTPUT);
+    digitalWrite(EN_PIN, HIGH);
+  }
+
+  void activateTimer(bool en) {
+    if (en == true && timerActive == false) {
+      sysclock.cancel(*this);
+      set(millis2ticks(WAKE_TIME_MS));
+      sysclock.add(*this);
+    } else if (en == false) {
+      sysclock.cancel(*this);
+    }
+    timerActive = en;
+ }
+
+  virtual void trigger(__attribute__((unused)) AlarmClock& clock) {
+    powerOff(true);
+  }
+
+  void powerOff(bool save) {
+    if (save == true) {
+      StorageConfig sc = sdev.getConfigArea();
+      uint8_t currentCount = sdev.nextcount() - 1;
+      sc.setByte(MSG_COUNT_BYTE, currentCount);
+      sc.validate();
+      DPRINT("setByte ");DDECLN(currentCount);
+    }
+    DPRINTLN(F("GOOD BYE JONNY"));_delay_ms(200);
+    digitalWrite(ACTIVATE_PIN, LOW);
+  }
+  
+};
+
+PowerOffAlarm<ACTIVATE_PIN> pwrOffAlarm;
+
 void setup () {
+  DINIT(57600,ASKSIN_PLUS_PLUS_IDENTIFIER);
+
   pinMode(ACTIVATE_PIN, OUTPUT);
   digitalWrite(ACTIVATE_PIN, HIGH);
-  pinMode(CC1101_PWR, OUTPUT);
-  digitalWrite(CC1101_PWR, LOW);
+  pinMode(CC1101_PWR_PIN, OUTPUT);
+  digitalWrite(CC1101_PWR_PIN, LOW);
 
-  DINIT(57600,ASKSIN_PLUS_PLUS_IDENTIFIER);
-  _delay_ms(200);
-  sdev.init(hal);
-  hal.battery.init(0,sysclock);
-  hal.battery.low(22);
-  hal.battery.critical(19);
+  pwrOffAlarm.init();
 
   buttonISR(cfgBtn,CONFIG_BTN);
   remoteISR(sdev,1,BTN01_PIN);
   remoteISR(sdev,2,BTN02_PIN);
+  
+  uint8_t TA = 0;
+  if (digitalRead(CONFIG_BTN) == LOW) TA = 1;
+  if (digitalRead(BTN01_PIN)  == LOW) TA = 2;
+  if (digitalRead(BTN02_PIN)  == LOW) TA = 3;
+  DPRINT("TA = ");DDECLN(TA);
 
-  while (hal.battery.current() == 0);
+  if (TA > 0) {
+    sdev.init(hal);
+    hal.battery.init(0,sysclock);
+    hal.battery.low(BAT_LOW);
+    hal.battery.critical(BAT_CRIT);
+    sdev.initDone();
 
-  sdev.initDone();
+    while (hal.battery.current() == 0);
+    if (hal.battery.critical()) pwrOffAlarm.powerOff(false);
+
+    StorageConfig sc = sdev.getConfigArea();
+    uint8_t msgCount = sc.getByte(MSG_COUNT_BYTE);
+    while (sdev.nextcount() < msgCount);
+  
+    hal.radio.setSendTimeout();
+    if (TA == 1) cfgBtn.irq();
+    else if (TA == 2) sdev.channel(1).button().irq();
+    else if (TA == 3) sdev.channel(2).button().irq();
+  } else {
+    pwrOffAlarm.powerOff(false);
+  }
 }
-
 void loop() {
   bool worked = hal.runready();
   bool poll = sdev.pollRadio();
-  if( worked == false && poll == false ) {
-    hal.activity.savePower<Sleep<>>(hal);
-  }
+  pwrOffAlarm.activateTimer( hal.activity.stayAwake() == false &&  worked == false && poll == false );
 }
